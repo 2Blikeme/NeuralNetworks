8cc28cdef0e0a369a95be718cd9fe8745330be73
d.marusev <banya12370@gmail.com>
2023-09-05 18:33:06 +0300

    init message

---
+++ src/main/kotlin/com/neural/nets/ns_first_lab/NsFirstLabApplication.kt
@@ -0,0 +1,11 @@
+package com.neural.nets.ns_first_lab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NsFirstLabApplication
+
+fun main(args: Array<String>) {
+    runApplication<NsFirstLabApplication>(*args)
+}
---
+++ src/test/kotlin/com/neural/nets/ns_first_lab/NsFirstLabApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.neural.nets.ns_first_lab
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NsFirstLabApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
